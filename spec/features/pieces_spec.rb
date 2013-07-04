8913b3319c1bbae42de4d363610ac85c17565919
Emily Ellison <emilyellison986@gmail.com>
2013-07-04 10:12:39 -0400

    pieces/index page

--- spec/features/pieces_spec.rb
+++ spec/features/pieces_spec.rb
@@ -1,6 +1,19 @@
 require 'spec_helper'
 
 feature "pieces" do
+  
+  before do
+    5.times { create(:piece) }
+    visit pieces_path
+  end
+  
+  scenario "guest should see a thumb of all pieces" do
+    page.should have_selector('img.thumb', count: 5)    
+  end
+  
+  scenario "guest should see one large piece" do
+    page.should have_selector('img.large', count: 1)    
+  end
 
 end
 
