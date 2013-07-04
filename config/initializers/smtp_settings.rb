1ee90c6b6eba0aaed0ed88422c539637445445d4
Emily Ellison <emilyellison986@gmail.com>
2013-07-04 16:16:36 -0400

    heroku prep

--- config/initializers/smtp_settings.rb
+++ config/initializers/smtp_settings.rb
@@ -1,13 +1,30 @@
-APP_CONFIG = YAML.load_file("#{Rails.root}/config/email.yml")[Rails.env]
+if Rails.env.production?
+  
+  ActionMailer::Base.default_url_options = { host: 'writ.herokuapp.com' }
+  ActionMailer::Base.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'fixitwithcode.com',
+    :user_name            => ENV['email'],
+    :password             => ENV['password'],
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  
+  }
+  
+else
 
-ActionMailer::Base.default_url_options = { host: APP_CONFIG['host'] }
+  APP_CONFIG = YAML.load_file("#{Rails.root}/config/email.yml")[Rails.env]
 
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => 'fixitwithcode.com',
-  :user_name            => APP_CONFIG['email'],
-  :password             => APP_CONFIG['password'],
-  :authentication       => 'plain',
-  :enable_starttls_auto => true  
-}
+  ActionMailer::Base.default_url_options = { host: APP_CONFIG['host'] }
+
+  ActionMailer::Base.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'fixitwithcode.com',
+    :user_name            => APP_CONFIG['email'],
+    :password             => APP_CONFIG['password'],
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  
+  }
+
+end
